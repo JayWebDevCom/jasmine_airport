a9d9f2bf46a34a5e8bfe9f6d0bceaf7d6794b5a2
JayWebDevCom <jaiye.s@gmail.com>
2017-05-10 07:08:43 +0100

    add improved revealing of intention

--- spec/airportspec.js
+++ spec/airportspec.js
@@ -9,28 +9,34 @@ describe('Airport', function(){
     });
 
   describe('knows its plane hangar array', function() {
-    it('is empty', function() {
+
+    it('is an array', function() {
       expect(heathrow._planes.constructor.name).toEqual('Array');
+    });
+
+    it('is empty', function() {
       expect(heathrow.how_many_planes()).toEqual(0);
     });
+
   });
 
   describe('knows its plane array', function() {
+
     it('receives a plane on #land', function() {
       heathrow.land(jet);
       expect(heathrow.how_many_planes()).toEqual(1);
     });
-  });
 
-  describe('knows its plane array', function() {
     it('releases a plane on #takeOff', function() {
       heathrow.land(jet);
       expect(heathrow.how_many_planes()).toEqual(1);
       heathrow.takeOff(jet);
       expect(heathrow.how_many_planes()).toEqual(0);
     });
+
   });
-})
+
+});
 
 
 // spyOn(Math, 'random').and.returnValues(0.1, 0.2, 0.3, 0.4);
